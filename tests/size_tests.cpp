9c91b0f20804c3b754634eb5699d77f9005d5466
arvidsson <arvidsson@gmail.com>
2017-03-18 12:40:09 +0100

    Added Size tests.

--- tests/size_tests.cpp
+++ tests/size_tests.cpp
@@ -2,9 +2,53 @@
 
 #include "../src/Size.h"
 
-TEST_CASE("test size", "[Size]")
+using namespace io;
+
+TEST_CASE("basics", "[Size]")
 {
-    io::Size2i s = { 100, 100 };
-    REQUIRE(s.w == 100);
-    REQUIRE(s.h == 100);
+    Size2i s0;
+    Size2i s1 = { 100, 100 };
+    Size2i s2(50, 50);
+    Size2i s3 = s1;
+    Size2i s4(s2);
+    Size2f sf1 = { 100.0f, 100.0f };
+    Size2f sf2 = sf1;
+    Size2f sf3 = { 50.0f, 50.0f };
+    
+    REQUIRE(s0.w == 0);
+    REQUIRE(s0.h == 0);
+    REQUIRE(s1.w == 100);
+    REQUIRE(s1.h == 100);
+    REQUIRE(s2.w == 50);
+    REQUIRE(s2.h == 50);
+    REQUIRE(s3.w == 100);
+    REQUIRE(s3.h == 100);
+    REQUIRE(s4.w == 50);
+    REQUIRE(s4.h == 50);
+    REQUIRE(s1 == s3);
+    REQUIRE(s1 != s2);
+    REQUIRE(sf1.is_equal(sf2));
+    REQUIRE(!sf1.is_equal(sf3));
+}
+
+TEST_CASE("maths", "[Size]")
+{
+    Size2i s = { 100, 100 };
+    Size2i s2 = { 50, 50 };
+    s += { 100, 100 };
+    REQUIRE(s == Size2i(200, 200));
+    s = s2 + s2;
+    REQUIRE(s == Size2i(100, 100));
+    s -= s2;
+    REQUIRE(s == Size2i(50, 50));
+    s = s - s * 2;
+    REQUIRE(s == Size2i(-50, -50));
+    s = -s;
+    REQUIRE(s == Size2i(50, 50));
+    s = s / 2;
+    REQUIRE(s == Size2i(25, 25));
+    s *= 5;
+    REQUIRE(s == Size2i(125, 125));
+    s /= 5;
+    REQUIRE(s == Size2i(25, 25));
 }
